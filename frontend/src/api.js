a0b7332491c4492dd3c95706185a11793b1e624b
nikhilkumarhack12 <nikhilkumarhack12@gmail.com>
2025-07-19 13:56:05 +0530

    added backend url

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
+const API_URL = process.env.REACT_APP_API_URL || 'https://vhelp-game.onrender.com/api';
 export const getRandomQuestions = async () => {
   const res = await axios.get(`${API_URL}/questions/random?count=5`);
   return res.data;
